abec418a670621cc6cb4f3066e801d2e1776f89c
Frank9401 <faxriddin0115@gmail.com>
2023-05-30 15:24:33 +0900

    feat:member controllerlarni qurish

--- router.js
+++ router.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const memberController = require("./controllers/memberController");
 
-router.get("/", function (req,res) {
-    res.send("home sahifasidasiz");
-});
+// memberga dahldol routerlar
+router.get("/", memberController.home);
+router.post("/signup", memberController.signup);
+router.post("/login", memberController.login);
+router.get("/logout", memberController.logout);
 
+// boshqa routerlar
 router.get("/menu", (req,res)=>{
     res.send("Menu sahifadasiz");
 });
@@ -12,5 +16,5 @@ router.get("/menu", (req,res)=>{
 router.get("/community", (req, res) => {
     res.send("Jamiyat sahifasidasiz");
 });
-
+ 
 module.exports = router;
