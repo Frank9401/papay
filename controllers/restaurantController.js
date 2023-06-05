3750b53a863867fb54e79883516c3d3f7cf295a9
Frank9401 <faxriddin0115@gmail.com>
2023-06-05 23:20:00 +0900

    feat:multer single and multi images upload and process

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -86,9 +86,22 @@ restaurantController.logout = (req, res) => {
     res.send("logout sahifadasiz");
 };
 
+
+restaurantController.validateAuthRestaurant = (req, res, next) => {
+    if(req.session?.member?.mb_type === "RESTAURANT") {
+        req.member = req.session.member;
+        next();
+    
+    } else 
+    res.json({
+        state: "fail", 
+        error: "only authenticated member with restaurant type",
+    });
+};
+
 restaurantController.checkSessions = (req, res) => {
    if(req.session?.member) {
-    res.json({state: 'succeed', data: req.session.member})
+    res.json({state: 'succeed', data: req.session.member});
    } else {
     res.json({state: 'fail', message: "You are not authenticated"});
    }
