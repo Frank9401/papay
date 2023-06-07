135f26ce417a724e844befa46ec8127e4e31f219
Frank9401 <faxriddin0115@gmail.com>
2023-06-08 05:04:38 +0900

    feat: F-Task

--- train.js
+++ train.js
@@ -16,11 +16,10 @@
 //   return maxProfit;
 // }
 
-
 // const detail_list = [32, null, 5, null, 13, null, 48, -78,];
 // const result1 = moveNullsKeepOrder(detail_list);
 
-// console.log("result:", result1); 
+// console.log("result:", result1);
 // function moveNullsKeepOrder(detail_list){
 //   return detail_list.sort((a, b) => {
 //   if (a === null) {
@@ -38,10 +37,7 @@
 //   return a < b ? -1 : 1;
 // });
 
-// } 
-
-
-
+// }
 
 // function Solution (n) {
 //     let a = String(n);
@@ -56,20 +52,25 @@
 //     return arr2[0];
 // }
 
+// Task-F
+// F-Task: Shunday function tuzing, 
+//unga string argument pass bolsin. 
+//Function ushbu agrumentdagi faqat digitlarni yangi stringda 
+//return qilsin!  
+//   Masalan: findDigits('ad5we34jkf89') return qilishi kerak bolgan qiymat '53489'
 
+ function findDigits(str) {
+  let digits = '';
 
-   function findDigits(str) {
-    let digits = '';
-    
-    for (let i = 0; i < str.length; i++) {
-      const char = str.charAt(i);
-      
-      if (/[0-9]/.test(char)) {
-        digits += char;
-      }
+  for (let i = 0; i < str.length; i++) {
+    const char = str.charAt(i);
+
+    if (/[0-9]/.test(char)) {
+      digits += char;
     }
-    
-    return digits;
   }
-  
-  console.log(findDigits('ad5we34jkf89'));
+
+  return digits;
+}
+
+console.log(findDigits('ad5we34jkf89'));
